573af101c8945233b3bb5740400543245bef3aaf
Tomáš Holas <tomas.holas@gmail.com>
2018-08-31 11:30:59 +0200

    first commit

---
+++ src/app/components/dummy.tsx
@@ -0,0 +1,7 @@
+import * as React from 'React';
+
+export class Dummy extends React.Component {
+  render() {
+    return <h1>Dummyx</h1>
+  }
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { hot } from 'react-hot-loader';
+import { Dummy } from './components/dummy'
+
+export const App = hot(module)(() => (
+  <Dummy />
+));
