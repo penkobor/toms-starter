9fa0b415620eac1e0288107d2fdd07292e676275
Tomáš Holas <tomas.holas@gmail.com>
2018-09-03 10:06:06 +0200

    login class

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -4,13 +4,16 @@ import { routeState } from 'app/routeState'
 import { Nav } from 'app/components/Nav'
 import { Foo } from 'app/components/Foo';
 
-export const Login = observer(() =>
-  <div>
-    <Nav />
-    <h3>Login Page</h3>
-    {routeState.loggedIn && <div>You are logged in! Visit some profile page :)</div>}
-    {routeState.loginRequiredMessage && <div>{routeState.loginRequiredMessage}</div>}
-    <Foo/>
-  </div>
-);
+@observer
+export class Login extends React.Component {
+  render() {
+    return <div>
+      <Nav />
+      <h3>Login Page</h3>
+      {routeState.loggedIn && <div>You are logged in! Visit some profile page :)</div>}
+      {routeState.loginRequiredMessage && <div>{routeState.loginRequiredMessage}</div>}
+      <Foo />
+    </div>
+  }
+}
 
